33be59401452c253ec98a3b2ad45170db56aa52c
Coa <beeki96@gmail.com>
2020-04-29 00:42:30 +0200

    Create custom javascript file, wordpress platform

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
+<!-- put code below in functions.php of your wordpress child theme -->
+
 <?php //register custom .js file in your wordpress child theme
 add_action('wp_enqueue_scripts', 'my_enqueue_front_scripts');
 function my_enqueue_front_scripts(){
