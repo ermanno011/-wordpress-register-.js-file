9e2c45b5c023f8cc95d3233ce305995affe01e05
Coa <beeki96@gmail.com>
2020-04-28 23:31:17 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php //register custom .js file in your wordpress child theme
+add_action('wp_enqueue_scripts', 'my_enqueue_front_scripts');
+function my_enqueue_front_scripts(){
+    wp_enqueue_script('my-custom-scripts', get_stylesheet_directory_uri().'/js/file.js');
+}
+?>
